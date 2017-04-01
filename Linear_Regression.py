c0e1ce9516efd671c77cd8c3a6e3a8f3a8a479f5
manish <manishch6652@gmail.com>
2017-04-01 12:16:20 +0530

    Linear Regression

---
+++ Linear_Regression.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import quandl #fiancial data python module
+df = quandl.get('WIKI/GOOGL') #google stock prices
+df = df[['Adj. Open','Adj. Low','Adj. High','Adj. Close','Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close'])/df['Adj. Close']*100;  #percent change in higher value
+df['DL_PCT'] = (df['Adj. Close'] - df['Adj. Open'])/df['Adj. Open']*100;  #Daily percent change 
+print(df.tail())
